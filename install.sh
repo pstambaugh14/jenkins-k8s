fce0c5dc57a803aef12a07b77071a41396161a65
Patrick Stambaugh <pstambaugh14@gmail.com>
2020-03-22 23:51:27 -0400

    post-transfer permissions needed

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 #ansible-playbook -vvvv playbook.yml
 #eval $(minikube docker-env)
+echo "Checking to see if namespace Jenkins exists already and deploying if not..."
 ./01-create-ns-jenkins.sh
+echo "Checking to see if persistent volume for Jenkins exists already and will leave it if so, create one if not..."
 ./02-create-volume-jenkins.sh
+echo "Checking to see if persistent volume claim for Jenkins exists already and will leave it if so, create one if not..."
 ./03-create-PVC-jenkins.sh
+echo "Checking to see if pods and deployments already exist for and will delete the previous if so, create new ones either way."
 ./04-create-deployment-jenkins.sh
+echo "Creating Service for Jenkins if they don't already exist..."
 ./05-create-service-jenkins.sh
+echo "Creating Service Accounts for Jenkins if they don't already exist..."
 ./06-create-SA-jenkins.sh
-clear
+#clear
+echo "Executing Post-Installation Scripts..."
+sleep 3
 #./environment
 kubectl rollout status deployment.v1.apps/jenkins-deployment -n jenkins && ./jenks_init_pw.sh
+sleep 3
+echo "Executing Validation scripts..."
 ./jenks_service_connect.sh
